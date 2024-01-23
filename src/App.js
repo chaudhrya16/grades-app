ac0f88095ba1a29f70fa722e34930577f64eac79
Anurag Chaudhry <anurag.chaudhry@pathccm.com>
2024-01-24 02:57:08 +0530

    full app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import GPAList from "./GPAList";
+import GradeList from "./GradeList";
+import AddGrade from "./AddGrade";
+import React, { useReducer } from "react";
+const initialState = {
+  grades: [
+    {
+      id: 1,
+      course: "AP Computer Science A",
+      grade: "9",
+      term: "Fall",
+      type: "AP",
+      letterGrade: "A",
+    },
+    {
+      id: 2,
+      course: "AP Computer Science A",
+      grade: "9",
+      term: "Spring",
+      type: "AP",
+      letterGrade: "B",
+    },
+    {
+      id: 3,
+      course: "Literature",
+      grade: "9",
+      term: "Fall",
+      type: "Regular",
+      letterGrade: "A",
+    },
+    {
+      id: 4,
+      course: "Literature",
+      grade: "9",
+      term: "Spring",
+      type: "Regular",
+      letterGrade: "C",
+    },
+    {
+      id: 5,
+      course: "Physics",
+      grade: "10",
+      term: "Fall",
+      type: "H",
+      letterGrade: "A",
+    },
+    {
+      id: 6,
+      course: "Physics",
+      grade: "11",
+      term: "Spring",
+      type: "H",
+      letterGrade: "C",
+    },
+    {
+      id: 7,
+      course: "AP Chem",
+      grade: "12",
+      term: "Fall",
+      type: "AP",
+      letterGrade: "C",
+    },
+    {
+      id: 8,
+      course: "AP Chem",
+      grade: "12",
+      term: "Spring",
+      type: "AP",
+      letterGrade: "A",
+    },
+  ],
+};
+export const Context = React.createContext(initialState);
+
+export const AppReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_GRADE":
+      console.log(action.payload);
+      return {
+        ...state,
+        grades: [action.payload, ...state.grades],
+      };
+    case "DELETE_GRADE":
+      return {
+        ...state,
+        grades: state.grades.filter((grade) => grade.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  function deleteGrade(id) {
+    dispatch({
+      type: "DELETE_GRADE",
+      payload: id,
+    });
+  }
+
+  function addGrade(grade) {
+    //console.log("in addGrade");
+    //console.log(grade);
+    dispatch({
+      type: "ADD_GRADE",
+      payload: grade,
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider
+      value={{
+        grades: state.grades,
+        deleteGrade: deleteGrade,
+        addGrade: addGrade,
+      }}
+    >
+      <GPAList />
+      <GradeList />
+      <AddGrade />
+    </Context.Provider>
   );
 }
 
